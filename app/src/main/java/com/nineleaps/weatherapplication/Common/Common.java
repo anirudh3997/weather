8988451231898c26a34f23107f2990ca39901e3b
anirudh3997 <anirudh.mangalvedhe@gmail.com>
2019-05-09 14:02:58 +0530

    Basic Layout with Location Permission

---
+++ app/src/main/java/com/nineleaps/weatherapplication/Common/Common.java
@@ -0,0 +1,8 @@
+package com.nineleaps.weatherapplication.Common;
+
+import android.location.Location;
+
+public class Common {
+    public static final String APP_ID = "e6e199bb6d867081f19c78798e9282fa";
+    public static Location current_location = null;
+}
---
+++ app/src/main/java/com/nineleaps/weatherapplication/Retrofit/RetrofitClient.java
@@ -0,0 +1,20 @@
+package com.nineleaps.weatherapplication.Retrofit;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static Retrofit instance;
+
+    public static Retrofit getInstance(){
+        if (instance == null){
+            instance = new Retrofit.Builder()
+                    .baseUrl("http://api.openweathermap.org/data/2.5/weather")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build();
+        }
+        return instance;
+    }
+}
